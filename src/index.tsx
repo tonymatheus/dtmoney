46a217c90d5e1ec75ba3be7b4804bbef91bfe9cb
tonymatheus <tony.chaves14@gmail.com>
2021-09-23 08:11:51 -0300

    Create routes in api to make the  registration a list of api

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,49 @@ import ReactDOM from "react-dom";
 
 import { App } from "./App";
 
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
+import Schema from "miragejs/orm/schema";
+import { title } from "process";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer  de Web  site",
+          type: "deposit",
+          category: "Development",
+          amount: 10000,
+          createdAt: new Date("2021-02-12 07:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "contas casa",
+          amount: 1500,
+          createdAt: new Date("2021-02-29 09:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "transaction",
-          amount: 400,
-          type: "deposit",
-          category: "food",
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
     });
   },
 });
